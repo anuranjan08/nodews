383214946d13041a789e0a1d2b963afe4c3da9ef
anuranjan <singh.anuranjan3@gmail.com>
2021-12-25 02:46:39 +0530

    modified

--- basic_http_server/index.js
+++ basic_http_server/index.js
@@ -5,7 +5,23 @@ const fs=require('fs');
 function requestHandler(req,res){
     console.log(req.url);
     res.writeHead(200,{'content-type':'text/html'});
-    fs.readFile('./index.html',function(err,data){
+
+    let filepath;
+
+    switch(req.url){
+        case '/':
+            filepath='./index.html';
+            break;
+        case '/profile':
+            filepath='./profile.html';
+            break;
+        default:
+            filepath='./404.html';
+
+    }
+
+
+  fs.readFile(filepath,function(err,data){
         if(err){
             console.log(err);
             return;
